48b82fd25e2ca1cd3ea04f7ff8f719100c4db74d
Andrés Tufiño <150483524+Andrespipe1@users.noreply.github.com>
2025-06-01 11:21:11 -0500

    Delete src/environments/environment.ts

--- src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  supabaseUrl: 'https://uzfjlndypjonnkugeaev.supabase.co',
-  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV6ZmpsbmR5cGpvbm5rdWdlYWV2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgyOTcxODAsImV4cCI6MjA2Mzg3MzE4MH0.UlYtqJEKF8C23bDFcNPXGXutBjVryalnm07onBRxxAY'
-};
